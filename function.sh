f87ed0c9eb8ea34683371c4fc1ba693ab460d552
tantakotan <planettako@gmail.com>
2017-03-20 20:12:01 +0900

    Create function.sh

---
+++ function.sh
@@ -0,0 +1,17 @@
+# functions.sh
+
+# Edit 20170319
+
+# bases variables
+v_date="$(date +%Y%m%d)"
+v_time="$(date +%H%M%S)"
+v_backup_suffix="_${v_date}_${v_time}.backup"
+v_log_file="${HOME}/tantakotan_${v_date}_${v_time}.log"
+
+# functions
+function LogInfo()
+{
+        echo $1 | awk '{print "'"$(date -Is)"' [INFO]: "$0}' | tee -a ${v_log_file}
+}
+
+# EOF
