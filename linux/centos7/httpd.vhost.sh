ed44b0a14c1d1701675491d6cd0805c4f0f9ab09
tantakotan <planettako@gmail.com>
2017-03-21 01:16:13 +0900

    Update httpd.vhost.sh

--- linux/centos7/httpd.vhost.sh
+++ linux/centos7/httpd.vhost.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -eu
+
 # Edit 20170319
 v_github_dir="raw.githubusercontent.com/tantakotan/tantakotan.github.io/master/linux"
 v_script_name="centos7/httpd.vhost.sh"
+v_functions="curl -fLRs "${v_github_dir}/functions.sh""
 
 # functions
-. <(curl -LRs "${v_github_dir}/functions.sh") || echo "$(date -Is) [ERROR]: Failed to load https://${v_github_dir}/functions.sh"
+. <(${v_functions}|| echo exit 11)
 
 {
-	LogInfo "Start \"${v_script_name}\"."
+        LogInfo "Start \"${v_script_name}\"."
         LogInfo "LogInfo"
-	LogNotice "LogNotice"
-	LogError "LogError"
+        LogNotice "LogNotice"
+        LogError "LogError"
+        echo "unko"
 }
